979f00603c63d6a9d0b5dd0bd2d89c294fb1c6a0
khushbu19998@outlook.com <khushbu19998@outlook.com>
2024-06-22 22:17:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [product, setproduct] = useState([
+    { id: 1, name: 'p1', quantity: 0 },
+    { id: 2, name: 'p2', quantity: 0 },
+    { id: 3, name: 'p3', quantity: 0 },
+    { id: 4, name: 'p4', quantity: 0 },
+  ])
+  const IncrementHandler = (id) => {
+    const increment = product.map((product) => {
+      if (product.id === id && product.quantity < 4) {
+        return { ...product, quantity: product.quantity + 1 }
+      }
+      return product;
+    });
+    setproduct(increment)
+  }
+  const DecrementtHandler = (id) => {
+    const decrement = product.map((product) => {
+      if (product.id === id && product.quantity >0) {
+        return { ...product, quantity: product.quantity - 1 }
+      }
+      return product;
+    });
+    setproduct(decrement)
+  }
+  const reserHandler = (id) => {
+    const decrement = product.map((product) => {
+      if (product.id === id && product.quantity >0) {
+        return { ...product, quantity: product.quantity =0 }
+      }
+      return product;
+    });
+    setproduct(decrement)
+  }
+  const gettotal =() =>{
+    const total=product.reduce((total,product)=>
+    total+product.quantity,0
+  );
+  return total;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h2>total<br/>{gettotal()}</h2>
+      {
+        product.map((product) => {
+          return (
+            <div className='flex'>
+              <p>{product.quantity}</p>
+              <button onClick={() => IncrementHandler(product.id)}>+</button>
+              <button onClick={() => DecrementtHandler(product.id)}>-</button>
+              <button onClick={()=>reserHandler(product.id)}>Reset</button>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
